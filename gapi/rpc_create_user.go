a50101500643f5ef4e3dbd7c57ed4cd4caabb018
emzhofb <ikhda@alterra.id>
2024-10-30 15:18:55 +0700

    simplebank lesson 76: graceful shutdown the server

--- gapi/rpc_create_user.go
+++ gapi/rpc_create_user.go
@@ -10,6 +10,7 @@ import (
 	"github.com/emzhofb/simplebank/val"
 	"github.com/emzhofb/simplebank/worker"
 	"github.com/hibiken/asynq"
+	"github.com/rs/zerolog/log"
 	"google.golang.org/genproto/googleapis/rpc/errdetails"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
@@ -47,6 +48,9 @@ func (server *Server) CreateUser(ctx context.Context, req *pb.CreateUserRequest)
 		},
 	}
 
+	log.Info().Msg(">> creating user ...")
+	time.Sleep(10 * time.Second)
+
 	txResult, err := server.store.CreateUserTx(ctx, arg)
 	if err != nil {
 		if db.ErrorCode(err) == db.UniqueViolation {
@@ -58,6 +62,8 @@ func (server *Server) CreateUser(ctx context.Context, req *pb.CreateUserRequest)
 	rsp := &pb.CreateUserResponse{
 		User: convertUser(txResult.User),
 	}
+
+	log.Info().Msg(">> user created ...")
 	return rsp, nil
 }
 
